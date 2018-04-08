169e2088dc99ffac223b7c1639d97267f2546230
Mike <michaelbrooke93@gmail.com>
2018-04-08 14:46:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# redux_test
+
+A test bed for getting redux working
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+const String _name = "Your Name";
+
+void main() {
+  runApp(new FriendlychatApp());
+}
+
+class FriendlychatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Friendlychat",
+      home: new ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  //modified
+  @override //new
+  State createState() => new ChatScreenState(); //new
+}
+
+// Add the ChatScreenState class definition in main.dart.
+
+class ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final TextEditingController _textController = new TextEditingController();
+
+  //new
+  @override //new
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Friendlychat")),
+      body: new Column(
+        //modified
+        children: <Widget>[
+          //new
+          new Flexible(
+            //new
+            child: new ListView.builder(
+              //new
+              padding: new EdgeInsets.all(8.0), //new
+              reverse: true, //new
+              itemBuilder: (_, int index) => _messages[index], //new
+              itemCount: _messages.length, //new
+            ), //new
+          ), //new
+          new Divider(height: 1.0), //new
+          new Container(
+            //new
+            decoration: new BoxDecoration(color: Theme.of(context).cardColor), //new
+            child: _buildTextComposer(), //modified
+          ), //new
+        ], //new
+      ), //new
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return new IconTheme(
+      //new
+      data: new IconThemeData(color: Theme.of(context).accentColor), //new
+      child: new Container(
+        //modified
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration: new InputDecoration.collapsed(hintText: "Send a message"),
+              ),
+            ),
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(icon: new Icon(Icons.send), onPressed: () => _handleSubmitted(_textController.text)),
+            ),
+          ],
+        ),
+      ), //new
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    ChatMessage message = new ChatMessage(
+      //new
+      text: text, //new
+    ); //new
+    setState(() {
+      //new
+      _messages.insert(0, message); //new
+    }); //new
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: new CircleAvatar(child: new Text(_name[0])),
+          ),
+          new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Text(_name, style: Theme.of(context).textTheme.subhead),
+              new Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: new Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
