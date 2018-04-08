e56c89e362e3d0a11942a3724f9d21a3d0cceba5
Mike <michaelbrooke93@gmail.com>
2018-04-08 15:11:55 +0200

    Removed all unnecessary const and new keywords, since we are using Dart 2

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class AppState {
   AppState({this.messages = const []});
 
   AppState copyWith({List<String> messages}) {
-    return new AppState(messages: messages ?? this.messages);
+    return AppState(messages: messages ?? this.messages);
   }
 
   @override
@@ -37,20 +37,20 @@ class AppState {
 
 //REDUCERS
 AppState appStateReducer(AppState state, dynamic action) {
-  return new AppState(messages: messagesReducer(state.messages, action));
+  return AppState(messages: messagesReducer(state.messages, action));
 }
 
-final messagesReducer = combineReducers<List<String>>([new TypedReducer<List<String>, OnMessageAddedAction>(_addMessage)]);
+final messagesReducer = combineReducers<List<String>>([TypedReducer<List<String>, OnMessageAddedAction>(_addMessage)]);
 
 List<String> _addMessage(List<String> messages, OnMessageAddedAction action) {
-  return new List.from(messages)..add(action.newMessage);
+  return List.from(messages)..add(action.newMessage);
 }
 
 //APP
 void main() {
-  final store = new Store<AppState>(appStateReducer);
+  final store = Store<AppState>(appStateReducer);
 
-  runApp(new FriendlychatApp(store));
+  runApp(FriendlychatApp(store));
 }
 
 class FriendlychatApp extends StatelessWidget {
@@ -60,41 +60,41 @@ class FriendlychatApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       title: "Friendlychat",
-      home: new ChatScreen(),
+      home: ChatScreen(),
     );
   }
 }
 
 class ChatScreen extends StatefulWidget {
   @override
-  State createState() => new ChatScreenState();
+  State createState() => ChatScreenState();
 }
 
 // Add the ChatScreenState class definition in main.dart.
 
 class ChatScreenState extends State<ChatScreen> {
   final List<ChatMessage> _messages = <ChatMessage>[];
-  final TextEditingController _textController = new TextEditingController();
+  final TextEditingController _textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(title: new Text("Friendlychat")),
-      body: new Column(
+    return Scaffold(
+      appBar: AppBar(title: Text("Friendlychat")),
+      body: Column(
         children: <Widget>[
-          new Flexible(
-            child: new ListView.builder(
-              padding: new EdgeInsets.all(8.0),
+          Flexible(
+            child: ListView.builder(
+              padding: EdgeInsets.all(8.0),
               reverse: true,
               itemBuilder: (_, int index) => _messages[index],
               itemCount: _messages.length,
             ),
           ),
-          new Divider(height: 1.0),
-          new Container(
-            decoration: new BoxDecoration(color: Theme.of(context).cardColor),
+          Divider(height: 1.0),
+          Container(
+            decoration: BoxDecoration(color: Theme.of(context).cardColor),
             child: _buildTextComposer(),
           ),
         ],
@@ -103,22 +103,22 @@ class ChatScreenState extends State<ChatScreen> {
   }
 
   Widget _buildTextComposer() {
-    return new IconTheme(
-      data: new IconThemeData(color: Theme.of(context).accentColor),
-      child: new Container(
-        margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: new Row(
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
           children: <Widget>[
-            new Flexible(
-              child: new TextField(
+            Flexible(
+              child: TextField(
                 controller: _textController,
                 onSubmitted: _handleSubmitted,
-                decoration: new InputDecoration.collapsed(hintText: "Send a message"),
+                decoration: InputDecoration.collapsed(hintText: "Send a message"),
               ),
             ),
-            new Container(
-              margin: new EdgeInsets.symmetric(horizontal: 4.0),
-              child: new IconButton(icon: new Icon(Icons.send), onPressed: () => _handleSubmitted(_textController.text)),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(icon: Icon(Icons.send), onPressed: () => _handleSubmitted(_textController.text)),
             ),
           ],
         ),
@@ -128,7 +128,7 @@ class ChatScreenState extends State<ChatScreen> {
 
   void _handleSubmitted(String text) {
     _textController.clear();
-    ChatMessage message = new ChatMessage(
+    ChatMessage message = ChatMessage(
       text: text,
     );
     setState(() {
@@ -142,22 +142,22 @@ class ChatMessage extends StatelessWidget {
   final String text;
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      margin: const EdgeInsets.symmetric(vertical: 10.0),
-      child: new Row(
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          new Container(
-            margin: const EdgeInsets.only(right: 16.0),
-            child: new CircleAvatar(child: new Text(_name[0])),
+          Container(
+            margin: EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(_name[0])),
           ),
-          new Column(
+          Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              new Text(_name, style: Theme.of(context).textTheme.subhead),
-              new Container(
-                margin: const EdgeInsets.only(top: 5.0),
-                child: new Text(text),
+              Text(_name, style: Theme.of(context).textTheme.subhead),
+              Container(
+                margin: EdgeInsets.only(top: 5.0),
+                child: Text(text),
               ),
             ],
           ),
